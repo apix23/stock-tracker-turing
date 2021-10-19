91af0969c3a134359b6c8948e37161e1d831b2b7
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-19 15:47:26 +0100

    fix: fixed the React scope error in eslint

--- src/utils/sum.test.js
+++ src/utils/sum.test.js
@@ -1,5 +1,13 @@
+import React from 'react'
+const { render } = require('@testing-library/react')
+const { default: App } = require('../App')
 const sum = require('./sum')
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test('testing app', () => {
+  render(<App />)
+  expect(true)
 })
