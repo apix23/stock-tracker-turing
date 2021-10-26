bd047dcbd2859e41f37006e1bd639da514ace2a3
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-26 17:09:50 +0100

    feat: added FTE indexes with price tracking and dynamic text color and symbol

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import SplashScreen from './components/SplashScreen'
 // import Graph from './components/Graph/Graph'
+// import Fte from './components/Fte/Fte'
 
 function App() {
   return (
     <div className="App">
       <SplashScreen />
       {/* <Graph /> */}
+      {/* <Fte /> */}
     </div>
   )
 }
