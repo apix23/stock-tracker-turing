5cdd240d9f88b6f246321f5c682c2a94272ac922
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-10-28 10:19:13 +0200

    feat: splashs creen and search screen are connected, suggestion panel is working correctly

--- src/components/StockOption.jsx
+++ src/components/StockOption.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './StockOption.css'
 
 const StockOption = ({ symbol, stockName, inputUser }) => {
-  const highLithingWords = (word, i) => {
+  const highlightMatch = (word, i) => {
     const regex = new RegExp(inputUser, 'gi')
     const textMatch = word.match(regex)
     const strongText = textMatch ? textMatch.toString() : ''
@@ -18,7 +18,7 @@ const StockOption = ({ symbol, stockName, inputUser }) => {
   return (
     <div>
       <div className='stock-suggested'>
-        {highLithingWords(symbol)} - {stockName.split(' ').map(highLithingWords)}
+        {highlightMatch(symbol)} - {stockName.split(' ').map(highlightMatch)}
       </div>
     </div>
   )
