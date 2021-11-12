c01737329df919be021e69e577aebf62377f715e
Umit Oner <umit.oner@weareadaptive.com>
2021-11-12 11:27:53 +0100

    fix: converted sandbox api to real api

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime.js'
-const umitToken = `?token=Tpk_9f8a1a489e684df8ad8a935fab4b3504`
+const token = import.meta.env.VITE_API_TOKEN
 
 export interface NewsData {
   headline: string
@@ -9,7 +9,7 @@ export interface NewsData {
 }
 
 export const fetchNews = async (stockSymbol: string) => {
-  const newsURL = `https://sandbox.iexapis.com/stable/stock/${stockSymbol}/news/last/3${umitToken}`
+  const newsURL = `https://cloud.iexapis.com/stable/stock/${stockSymbol}/news/last/3${token}`
   try {
     const newsData: NewsData[] = await fetch(newsURL).then((res) => res.json())
     return newsData
