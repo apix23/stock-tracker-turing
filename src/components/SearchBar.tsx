69781b026028ecd028de6aba1ea65a574c5352ad
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-11-18 17:29:35 +0100

    feat: creation of the useKeyPress hook

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import SuggestionPanel from './SuggestionPanel/SuggestionPanel'
 import { queryFetch, QueryMarket } from '../services/queryService'
 import { QueryContext } from '../context/QueryContext'
+import { useKeyPress } from '../hooks/useKeyPress'
 
 interface SearchBarProps {
   style: React.CSSProperties
@@ -13,6 +14,13 @@ const SearchBar = ({ style = {}, currentResult }: SearchBarProps) => {
   const [stockSearch, setStockSearch] = useState('')
   const [isFocus, setIsFocus] = useState(false)
   const ref = useRef<HTMLInputElement>(null)
+
+  const [selected, setSelected] = useState(undefined)
+  const downPress = useKeyPress('ArrowDown')
+  const upPress = useKeyPress('ArrowUp')
+  const enterPress = useKeyPress('Enter')
+  const [cursor, setCursor] = useState(0)
+  const [hovered, setHovered] = useState(undefined)
 
   useEffect(() => {
     ref.current?.blur()
