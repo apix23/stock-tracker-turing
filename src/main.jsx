c34ac5057dead8cbe7c1e8015257d85de27c4d9d
Umit Oner <umit.oner@weareadaptive.com>
2021-11-18 17:59:30 +0100

    feat: first step into pwa world

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-console.log(import.meta.env.BASE_URL)
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
