b0fa6f49c64f7fd0eba4ca62613d3c225deb6afa
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-11-23 10:21:25 +0100

    fix: merging upstream changes into the branch

---
+++ src/components/SearchBar.test.tsx
@@ -0,0 +1,5 @@
+import SearchBar from './SearchBar'
+
+describe('when writing any text in the text bar', () => {
+  it('displays the word "Stocks" when there are results for the search', () => {})
+})
--- src/utils/highLightMatch..test.ts
+++ src/utils/highLightMatch..test.ts
@@ -1,19 +1,21 @@
 import { highlightMatch } from './highLightMatch'
 
-it('the text should matched should be returned', () => {
-  const [storngText] = highlightMatch('AAPL', 'AA')
+describe('when writing any text in the input', () => {
+  it('the text matched should be returned', () => {
+    const [strongText] = highlightMatch('AAPL', 'AA')
 
-  expect(storngText).toBe('AA')
-})
+    expect(strongText).toBe('AA')
+  })
 
-it('the text that doe not match should be returned as well', () => {
-  const [_, normalText] = highlightMatch('AAPL', 'AA')
+  it('the text that does not match should be returned as well', () => {
+    const [_, normalText] = highlightMatch('AAPL', 'AA')
 
-  expect(normalText).toBe('PL')
-})
+    expect(normalText).toBe('PL')
+  })
 
-it('if there is not match, the entired text should be returned', () => {
-  const [_, normalText] = highlightMatch('Corp', 'AAPL')
+  it('if there is no match, the entire text should be returned', () => {
+    const [_, normalText] = highlightMatch('Corp', 'AAPL')
 
-  expect(normalText).toBe('Corp')
+    expect(normalText).toBe('Corp')
+  })
 })
