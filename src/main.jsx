409ef71fa24b59e6d764f16cdabdab65b460fb56
Umit Oner <umit.oner@weareadaptive.com>
2021-11-23 10:04:46 +0100

    fix: generated manifest errors and file loactions

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { VFC } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { useRegisterSW } from 'virtual:pwa-register/react'
+
+const RegisterSW = () => {
+  useRegisterSW({ onRegistered: () => console.log('SW Registered') })
+  return <></>
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <RegisterSW />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
