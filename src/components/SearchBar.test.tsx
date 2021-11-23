42eed09676df560e9d61b1d7006a9b1bda50e1f1
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-11-23 08:57:01 +0100

    feat: initial tests for the search bar

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -1,5 +1,16 @@
 import SearchBar from './SearchBar'
 
 describe('when writing any text in the text bar', ()=>{
-    it('displays the word "Stocks" when there are results for the search', () => {})
+    it('displays the word "Stocks" when there are results for the search', () => {
+
+    })
+
+    it('displays a matching result highlighted with a gray background when there are results for the search', () => {
+
+    })
+
+    it('shows the text "no results found" when there are no results', () => {
+         
+    })
+
 })
