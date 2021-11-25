6ebcf2776952285ffd18ec2650461cbc2d894385
Timur Jalilov <timurjalilov1@gmail.com>
2021-11-25 12:14:38 +0000

    fix: fixes graph responsiveness and added scrolling

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -5,6 +5,7 @@ import GraphLoading from './GraphLoading'
 import GraphFailedToLoad from './GraphError'
 import useFetchGraphClose from '../../hooks/useFetchGraphClose'
 import useFetchGraphData from '../../hooks/useFetchGraphData'
+import { useDrag } from './useDrag'
 import { filterZero } from '../../utils/filterZero'
 import './Graph.css'
 
@@ -21,6 +22,7 @@ const Graph = ({ stockSymbol }: GraphProps) => {
   const [yesterdayClose] = useFetchGraphClose(yesterdayCloseUrl)
   const [yesterdayData, yesterdayDataError] = useFetchGraphData(yesterdayDataUrl)
   const [liveData, liveDataError] = useFetchGraphData(liveDataUrl)
+  const [reference, startDrag] = useDrag()
 
   if (liveDataError || yesterdayDataError || liveData?.length === 0) {
     return <GraphFailedToLoad />
@@ -31,61 +33,65 @@ const Graph = ({ stockSymbol }: GraphProps) => {
   }
 
   return (
-    <div className='chart'>
-      <ResponsiveContainer width='100%' height='100%'>
-        <LineChart data={yesterdayData}>
-          <CartesianGrid stroke='#d1d1d1' strokeWidth={0.4} verticalFill={['#ffffff00', '#ededed80']} />
+    <div className='chart' ref={reference} onMouseDown={startDrag}>
+      <div className='chart-inner'>
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart data={yesterdayData}>
+            <CartesianGrid stroke='#d1d1d1' strokeWidth={0.4} verticalFill={['#ffffff00', '#ededed80']} />
 
-          <YAxis
-            stroke='#eaebeb'
-            tickSize={10}
-            tickCount={12}
-            allowDecimals={true}
-            domain={['auto', 'auto']}
-            padding={{ top: 18 }}
-            dx={-5}
-            tick={<CustomizedAxisTick />}
-          />
+            <YAxis
+              stroke='#eaebeb'
+              tickSize={10}
+              tickCount={12}
+              allowDecimals={true}
+              domain={['auto', 'auto']}
+              padding={{ top: 18 }}
+              dx={-5}
+              tick={<CustomizedAxisTick />}
+            />
 
-          <XAxis
-            stroke='#eaebeb'
-            tickSize={10}
-            tickCount={12}
-            tick={{ fill: '#7f7f7f', fontSize: 12, fontFamily: 'Roboto' }}
-            interval={5}
-            allowDuplicatedCategory={false}
-            dataKey='label'
-            dy={5}
-            textAnchor='beginning'
-          />
+            <XAxis
+              stroke='#eaebeb'
+              tickSize={10}
+              tickCount={12}
+              tick={{ fill: '#7f7f7f', fontSize: 12, fontFamily: 'Roboto' }}
+              interval={5}
+              allowDuplicatedCategory={false}
+              dataKey='label'
+              dy={5}
+              textAnchor='beginning'
+            />
 
-          <Tooltip contentStyle={{ fontSize: 14, borderColor: '#aaabd1', textAlign: 'center', fontFamily: 'Roboto' }} />
+            <Tooltip
+              contentStyle={{ fontSize: 14, borderColor: '#aaabd1', textAlign: 'center', fontFamily: 'Roboto' }}
+            />
 
-          <ReferenceLine y={yesterdayClose?.close} stroke='#aaabd1' strokeDasharray='5 3' />
+            <ReferenceLine y={yesterdayClose?.close} stroke='#aaabd1' strokeDasharray='5 3' />
 
-          <Line
-            hide={false}
-            name='Close'
-            data={filterZero(yesterdayData)}
-            dataKey='close'
-            stroke='#aaabd1'
-            strokeWidth={2}
-            dot={false}
-            activeDot={{ r: 4 }}
-          />
+            <Line
+              hide={false}
+              name='Close'
+              data={filterZero(yesterdayData)}
+              dataKey='close'
+              stroke='#aaabd1'
+              strokeWidth={2}
+              dot={false}
+              activeDot={{ r: 4 }}
+            />
 
-          <Line
-            hide={true}
-            name='Yesterday Close'
-            data={filterZero(yesterdayData)}
-            dataKey='close'
-            stroke='grey'
-            strokeWidth={2}
-            dot={false}
-            activeDot={{ r: 4 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+            <Line
+              hide={true}
+              name='Yesterday Close'
+              data={filterZero(yesterdayData)}
+              dataKey='close'
+              stroke='grey'
+              strokeWidth={2}
+              dot={false}
+              activeDot={{ r: 4 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   )
 }
