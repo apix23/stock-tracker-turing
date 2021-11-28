426b55ca856f708d2982013c11c6c110521d01a3
Umit Oner <umit.oner@weareadaptive.com>
2021-11-28 17:34:55 +0100

    chore: right section slidein, main section slideup animation

--- src/components/TopPeers/TopPeers.tsx
+++ src/components/TopPeers/TopPeers.tsx
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom'
 import './TopPeers.css'
 
 export const Peers = ({ stockSymbol }: { stockSymbol: string | undefined }) => {
-  const token = 'sandbox_c5rtke2ad3ibf61ruc9g'
-  const peersURL = `https://finnhub.io/api/v1/stock/peers?symbol=${stockSymbol}&token=${token}`
-
+  // const token = 'sandbox_c5rtke2ad3ibf61ruc9g'
+  // const peersURL = `https://finnhub.io/api/v1/stock/peers?symbol=${stockSymbol}&token=${token}`
+  const token = `?token=Tpk_9f8a1a489e684df8ad8a935fab4b3504`
+  const peersURL = `https://sandbox.iexapis.com/stable/stock/${stockSymbol}/peers${token}`
   const [peers, error] = useFetchTopPeers(peersURL)
 
   if (error || peers?.length === 0) {
